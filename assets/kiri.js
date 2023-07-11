7f1c8a090afd620272009264e72298a85bb6abf6
Jasper Smit <jasper.smit@zonneplan.nl>
2023-07-11 11:37:52 +0200

    remove old gitgraph test from kiri.js

--- assets/kiri.js
+++ assets/kiri.js
@@ -26,25 +26,6 @@ var selected_view = "schematic";
 
 var is_fullscreen = false;
 
-// Git graph
-
-const graphContainer = document.getElementById("gitgraph");
-const gitgraph = createGitgraph(graphContainer);
-
-const master = gitgraph.branch("master");
-master.commit("Init the project");
-master
-  .commit("Add README")
-  .commit("Add tests")
-  .commit("Implement feature");
-master.tag("v1.0");
-const newFeature = gitgraph.branch("new-feature");
-newFeature.commit("Implement an awesome feature");
-master.commit("Hotfix a bug");
-newFeature.commit("Fix tests");
-// Merge `newFeature` into `master`
-master.merge(newFeature, "Release new version");
-
 // =======================================
 // HANDLE SHORTCUTS
 // =======================================
